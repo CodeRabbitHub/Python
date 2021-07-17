0c6341df55fc43fa6316ff664be779be5bedeaec
luckyrabbit85 <amanroland@gmail.com>
2021-07-18 00:57:13 +0530

    update

---
+++ Python/OOPs/15- Multiple inheritance.py
@@ -0,0 +1,17 @@
+class  Employee:
+    def greet(self):
+        print("Employee Greet")
+
+class Manager:
+    def greet(self):
+        print("Manager Greet")
+
+class Person(Employee, Manager):
+    pass
+
+p = Person()
+p.greet()
+
+# Changing order in inheritance changes the greet
+# Multiple inheritance is good when the two classes
+# have  no methods in common.
