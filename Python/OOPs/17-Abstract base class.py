44a6b217ecb7515eb5447bccf69e2019c99d1b97
luckyrabbit85 <amanroland@gmail.com>
2021-07-18 21:50:19 +0530

    update

--- Python/OOPs/17-Abstract base class.py
+++ Python/OOPs/17-Abstract base class.py
@@ -1,7 +1,9 @@
+from abc import ABC, abstractmethod
+
 class InvalidOperationError(Exception):
     pass
 
-class Stream:
+class Stream(ABC):
     def __init__(self):
         self.opened = False
     
@@ -15,6 +17,10 @@ class Stream:
             raise InvalidOperationError("Stream is already closed")
         self.opened = False
 
+    @abstractmethod
+    def read(self):
+        pass
+
 class FileStream(Stream):
     def read(self):
         print("Reading data from a file")
@@ -22,3 +28,11 @@ class FileStream(Stream):
 class NetworkStream(Stream):
     def read(self):
         print("Reading data from a Network")
+
+class MemoryStream(Stream):
+    def read(self):
+        print("Reading data from Memory")
+
+
+stream = MemoryStream()
+stream.open()
