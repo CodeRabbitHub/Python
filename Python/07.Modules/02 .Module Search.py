8431b21553e8c7106c7ea10cced2ef10669fda29
luckyrabbit85 <amanroland@gmail.com>
2021-07-23 17:24:22 +0530

    update

---
+++ Python/07.Modules/02 .Module Search.py
@@ -0,0 +1,7 @@
+import sales
+import sys
+
+print(sys.path)
+
+# python searches all these directories
+# for the module.
---
+++ Python/07.Modules/ecommerce/shopping/sales.py
@@ -0,0 +1,6 @@
+def calc_tax():
+    pass
+
+
+def calc_shipping():
+    pass
