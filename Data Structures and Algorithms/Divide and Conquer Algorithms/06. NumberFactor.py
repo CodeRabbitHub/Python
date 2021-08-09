ef4e17ec5b7ce1754e1c20e5d0dfb45a7dfeea5a
luckyrabbit85 <amanroland@gmail.com>
2021-08-09 20:20:42 +0530

    update

---
+++ Data Structures and Algorithms/Divide and Conquer Algorithms/06. NumberFactor.py
@@ -0,0 +1,13 @@
+def numberFactor(n):
+    if n in (0, 1, 2):
+        return 1
+    elif n == 3:
+        return 2
+    else:
+        subP1 = numberFactor(n - 1)
+        subP2 = numberFactor(n - 3)
+        subP3 = numberFactor(n - 4)
+        return subP1 + subP2 + subP3
+
+
+print(numberFactor(5))
---
+++ Data Structures and Algorithms/Divide and Conquer Algorithms/07. HouseRobber.py
@@ -0,0 +1,11 @@
+def houseRobber(houses, currentIndex):
+    if currentIndex >= len(houses):
+        return 0
+    else:
+        stealFirstHouse = houses[currentIndex] + houseRobber(houses, currentIndex + 2)
+        skipFirstHouse = houseRobber(houses, currentIndex + 1)
+        return max(stealFirstHouse, skipFirstHouse)
+
+
+houses = [6, 7, 1, 30, 8, 2, 4]
+print(houseRobber(houses, 0))
