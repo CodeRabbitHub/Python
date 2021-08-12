c655324bf3dc85847c74beac92844cd9e78bc040
luckyrabbit85 <amanroland@gmail.com>
2021-08-12 19:39:52 +0530

    update

---
+++ Leetcode Problems/2. Add Two Numbers.py
@@ -0,0 +1,46 @@
+# You are given two non-empty linked lists representing two non-negative integers.
+# The digits are stored in reverse order, and each of their nodes contains a single digit.
+# Add the two numbers and return the sum as a linked list.
+
+
+class ListNode:
+    def __init__(self, val=None, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        headNode = ListNode()
+        pointer = headNode
+        carry, suM = 0, 0
+
+        while l1 or l2:
+            suM = carry
+            if l1:
+                suM += l1.val
+                l1 = l1.next
+            if l2:
+                suM += l2.val
+                l2 = l2.next
+
+            carry = suM // 10
+            s = suM % 10
+            pointer.next = ListNode(s)
+            pointer = pointer.next
+
+        if carry > 0:
+            pointer.next = ListNode(carry)
+
+        return headNode.next
+
+
+l1 = ListNode(2, ListNode(4, ListNode(3)))
+l2 = ListNode(5, ListNode(6, ListNode(4)))
+
+s = Solution()
+result = s.addTwoNumbers(l1, l2)
+
+while result:
+    print(result.val)
+    result = result.next
---
+++ Leetcode Problems/3. Longest Substring Without Repeating Characters.py
@@ -0,0 +1,31 @@
+# Given a string s, find the length of the longest
+# substring without repeating characters.
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        hashmap = {}
+        left = 0
+        right = 0
+        max_len = 0
+
+        while left < len(s) and right < len(s):
+            char = s[right]
+            if char in hashmap:
+                left = max(left, hashmap[char] + 1)
+            hashmap[char] = right
+            max_len = max(max_len, right - left + 1)
+            right += 1
+        return max_len
+
+
+s1 = "abcabcbb"
+s2 = "bbbbb"
+s3 = "pwwkew"
+s4 = ""
+
+s = Solution()
+print(s.lengthOfLongestSubstring(s1), end="\n")
+print(s.lengthOfLongestSubstring(s2), end="\n")
+print(s.lengthOfLongestSubstring(s3), end="\n")
+print(s.lengthOfLongestSubstring(s4), end="\n")
