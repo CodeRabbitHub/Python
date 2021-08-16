54bbe5c74bcdb9b1ed38a9fc478c58bb4f65a489
luckyrabbit85 <amanroland@gmail.com>
2021-08-16 23:29:56 +0530

    update

--- Leetcode Problems/0015.3Sum.py
+++ Leetcode Problems/0015.3Sum.py
@@ -3,27 +3,37 @@
 # nums[i] + nums[j] + nums[k] == 0.
 # Notice that the solution set must not contain duplicate triplets.
 
-# class Solution:
-#     def threeSum(self, nums: list[int]) -> list[list[int]]:
-#         nums.sort()
-#         result = []
-#         i = 0
-#         while nums[i] < 0:
-#             temp = self.twosum(nums[i+1:], - i)
-#             if temp:
-#                 temp.extend(i)
-#                 result.append(temp)
-#             i += 1
-#         return result
+
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        result = []
+
+        for i, x in enumerate(nums):
+            if i > 0 and x == nums[i - 1]:
+                continue
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                threeSum = x + nums[left] + nums[right]
+                if threeSum > 0:
+                    right -= 1
+                elif threeSum < 0:
+                    left += 1
+                else:
+                    result.append([x, nums[left], nums[right]])
+                    left += 1
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+
+        return result
 
 
-def twoSum(nums, target):
-    dix = set()
-    for num in nums:
-        complement = target - num
-        if complement in dix:
-            return [complement, num]
-        dix.add(num)
+nums1 = [-1, 0, 1, 2, -1, -4]
+nums2 = []
+nums3 = [0]
 
+s = Solution()
 
-print(twoSum([2, 3, 4, 5], 56))
+print(s.threeSum(nums1))
+print(s.threeSum(nums2))
+print(s.threeSum(nums3))
