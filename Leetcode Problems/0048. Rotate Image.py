7e8039502edd9d94bb60a98b59568d55fee0651f
luckyrabbit85 <amanroland@gmail.com>
2021-08-20 17:12:50 +0530

    update

---
+++ Leetcode Problems/0048. Rotate Image.py
@@ -0,0 +1,19 @@
+# You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        self.transpose(matrix)
+        self.reflect(matrix)
+
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+    def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
---
+++ Leetcode Problems/0054. Spiral Matrix.py
@@ -0,0 +1,34 @@
+# Given an m x n matrix, return all elements of the matrix in spiral order.
+
+
+class Solution:
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+        result = []
+        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+
+        while left < right and top < bottom:
+            # get elements in the top row
+            for i in range(left, right):
+                result.append(matrix[top][i])
+            top += 1
+
+            # get elements in the right column
+            for i in range(top, bottom):
+                result.append(matrix[i][right - 1])
+            right -= 1
+
+            if not (left < right and top < bottom):
+                break
+
+            # get elements in the bottom row
+            for i in range(right - 1, left - 1, -1):
+                result.append(matrix[bottom - 1][i])
+            bottom -= 1
+
+            # get elements in the left column
+            for i in range(bottom - 1, top - 1, -1):
+                result.append(matrix[i][left])
+            left += 1
+
+        return result
