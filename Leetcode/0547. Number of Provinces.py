017ba6dceead668d7fc4c6b88092567e84c3df0e
luckyrabbit85 <amanroland@gmail.com>
2021-09-07 20:30:44 +0530

    update

--- Leetcode/0547. Number of Provinces.py
+++ Leetcode/0547. Number of Provinces.py
@@ -1,20 +1,51 @@
 class Solution:
     def findCircleNum(self, isConnected: list[list[int]]) -> int:
-        numNodes = len(isConnected)
-        id = [i for i in range(numNodes)]
 
-        def connected(p, q):
-            return id[p] == id[q]
+        # Find the number of vertices
+        N = len(isConnected)
 
-        def union(p, q):
-            pid, qid = id[p], id[q]
-            for i in range(n):
-                if id[i] == qid:
-                    id[i] = pid
+        # create space for storing root and rank
+        root = [i for i in range(N)]
+        rank = [1] * N
 
-        for x in range(n):
-            for y in range(x + 1, n):
-                if isConnected[x][y]:
-                    union(x, y)
+        # Path Compression Optimization for find function.
+        # After finding the root node, we can update the parent node
+        # of all traversed elements to their root node.
 
-        return len(set(id))
+        def find(x):
+            if x == root[x]:
+                return x
+            root[x] = find(root[x])
+            return root[x]
+
+        # union by rank, we choose the root node of the vertex with a larger `rank`
+        # We will merge the shorter tree under the taller tree and assign the root
+        # node of the taller tree as the root node for both vertices.
+
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            if rootX != rootY:
+                if rank[rootX] > rank[rootY]:
+                    root[rootY] = rootX
+                elif rank[rootX] < rank[rootY]:
+                    root[rootX] = rootY
+                else:
+                    root[rootY] = rootX
+                    rank[rootX] += 1
+
+        # Joining the vertices
+        for i in range(N):
+            for j in range(i + 1, N):
+                if isConnected[i][j] == 1:
+                    x = find(i)
+                    y = find(j)
+                    if x != y:
+                        union(x, y)
+
+        # Finding the roots
+        for i in range(N):
+            find(i)
+
+        # Number of roots present
+        return len(set(root))
